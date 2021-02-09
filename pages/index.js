060030c1bacf6a0165558b33261e10ef2acae281
Kiko <kikollovet@gmail.com>
2021-02-09 18:02:29 -0300

    Aula 3 feita

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import styled from 'styled-components'
+import Menu from '../src/components/commons/Menu'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-function Hello(props) {
-  return <h1>{props.children}</h1>
-}
 export default function Home() {
-  return <Title><Hello>Oi</Hello>My page</Title>
+  return(
+    <div>
+      <Menu />
+    </div>
+  )
 }
