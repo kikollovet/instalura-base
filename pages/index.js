3e3665853116fb52ab7436a276aaeb060b5d6aac
Kiko <kikollovet@gmail.com>
2021-02-09 12:43:40 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
-
+function Hello(props) {
+  return <h1>{props.children}</h1>
+}
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title><Hello>Oi</Hello>My page</Title>
 }
