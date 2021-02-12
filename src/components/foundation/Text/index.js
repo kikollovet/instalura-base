5166e0dcff132d7e632efe569fe1d3947139685f
Kiko <kikollovet@gmail.com>
2021-02-12 13:28:18 -0300

    Modulo 1 Aula 08

--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 import get from 'lodash/get';
+import { propToStyle } from '../../../theme/utils/propToStyle';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 //É possivel tirar o primeiro CSS e depois o ${}
 const paragraph1 = css`
@@ -24,11 +26,28 @@ const smallestException = css`
 export const TextStyleVariantsMap = {
     smallestException,
     paragraph1,
+    title: css`
+    ${({ theme }) => css`
+      font-size: ${theme.typographyVariants.titleXS.fontSize};
+      font-weight: ${theme.typographyVariants.titleXS.fontWeight};
+      line-height: ${theme.typographyVariants.titleXS.lineHeight};
+    `}
+    ${breakpointsMedia({
+      md: css`
+        ${({ theme }) => css`
+          font-size: ${theme.typographyVariants.title.fontSize};
+          font-weight: ${theme.typographyVariants.title.fontWeight};
+          line-height: ${theme.typographyVariants.title.lineHeight};
+        `}
+      `,
+    })}
+  `,
   };
 
 const TextBase = styled.span`
      ${({ variant }) => TextStyleVariantsMap[variant]}
      color: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+     ${propToStyle('textAlign')}
 `;
 
 export default function Text({ tag, variant, children, ...props }) {
@@ -51,5 +70,5 @@ Text.defaultProps = {
 Text.propTypes = {
     children: PropTypes.node.isRequired,
     tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
-    variant: PropTypes.oneOf(['paragraph1', 'smallestException']),
+    variant: PropTypes.oneOf(['title', 'paragraph1', 'smallestException']),
 };
---
+++ src/theme/utils/propToStyle.js
@@ -0,0 +1,55 @@
+import { css } from 'styled-components';
+import { breakpoints } from '..';
+import { breakpointsMedia } from './breakpointsMedia';
+
+export function propToStyle(propName) {
+  return (props) => {
+    const propValue = props[propName];
+
+    // if (typeof propValue === 'object') {
+    //   return css`
+    //     ${breakpointsMedia({
+    //         ...(propValue.xs && {
+    //             xs: { [propName]: propValue.xs },
+    //         }),
+    //         ...(propValue.sm && {
+    //             sm: { [propName]: propValue.sm },
+    //         }),
+    //         ...(propValue.md && {
+    //             md: { [propName]: propValue.md },
+    //         }),
+    //         ...(propValue.lg && {
+    //             lg: { [propName]: propValue.lg },
+    //         }),
+    //         ...(propValue.xl && {
+    //             xl: { [propName]: propValue.xl },
+    //         }),
+    //     })}
+    //   `;
+    // }
+
+    if (typeof propValue === 'object') {
+        return breakpointsMedia({
+            xs: {
+                [propName]: propValue.xs
+            },
+            sm: {
+                [propName]: propValue.sm
+            },
+            md: {
+                [propName]: propValue.md
+            },
+            lg: {
+                [propName]: propValue.lg
+            },
+            xl: {
+                [propName]: propValue.xl
+            },
+        })
+    }
+
+    return {
+        [propName]: props[propName],
+    };
+  };
+} 
