4d98483b9879ec69708c994980d0f1199f1caf46
Kiko <kikollovet@gmail.com>
2021-02-12 20:18:39 -0300

    Mais um teste

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,8 @@ export default function Home() {
             md: 'left',
           }}
         >
-          Lorem Ipsum  is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum  has been the industrys standard dummy text ever since the 1500s.
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
         </Text>
 
         <Button
